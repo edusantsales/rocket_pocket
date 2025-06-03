a1d3811a3adc3ef29d067ae21b1a9e0bbfa9b9bb
Eduardo Sant'Ana <edusantsales@gmail.com>
2025-06-03 16:23:11 -0300

    improvement: Separeted RocketPocketApp from main.dart and remove NativeSplash on the final initState method in the HomeView

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,10 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-import 'home_view.dart';
+import 'app.dart';
 
 void main() {
   final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  scheduleMicrotask(() async {
-    await Future<void>.delayed(const Duration(seconds: 3));
-    FlutterNativeSplash.remove();
-  });
   runApp(const RocketPocketApp());
-}
-
-class RocketPocketApp extends StatelessWidget {
-  const RocketPocketApp({super.key});
-
-  static Color get primaryColor => const Color(0xFF9556F6);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const HomeView(),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
-        primaryColor: primaryColor,
-        useMaterial3: true,
-      ),
-      title: 'Rocket Pocket',
-    );
-  }
 }
