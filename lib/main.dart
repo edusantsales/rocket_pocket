fdffda1df124836ed38bfb1940f13afe3b762f8e
Eduardo Sant'Ana <edusantsales@gmail.com>
2025-06-03 22:43:26 -0300

    improvement: Improvement on the time when remove Native Splash

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'app.dart';
 
-void main() {
+void main() async {
   final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const RocketPocketApp());
+  await Future<void>.delayed(Durations.extralong4 * 10);
+  FlutterNativeSplash.remove();
 }
