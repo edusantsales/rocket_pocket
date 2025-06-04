7c43be50921f07e5c1df6677f1173a7ef28d64b9
Eduardo Sant'Ana <edusantsales@gmail.com>
2025-06-03 22:39:44 -0300

    feat: Added env.dart and launch environments configurations setted in env.json

---
+++ lib/env.dart
@@ -0,0 +1,21 @@
+class Env {
+  static const Map<String, String> _keys = <String, String>{
+    'BASE_URL': String.fromEnvironment('BASE_URL'),
+    'LOGIN': String.fromEnvironment('LOGIN'),
+    'CATALOG': String.fromEnvironment('CATALOG'),
+    'MY_CONTENT': String.fromEnvironment('MY_CONTENT'),
+  };
+
+  static String _fromJson(String key) {
+    final String value = _keys[key] ?? '';
+    if (value.isEmpty) {
+      throw Exception('Environment variable $key is not set');
+    }
+    return value;
+  }
+
+  static String get baseUrl => _fromJson('BASE_URL');
+  static String get login => _fromJson('LOGIN');
+  static String get catalog => _fromJson('CATALOG');
+  static String get myContent => _fromJson('MY_CONTENT');
+}
